32d2c5c6e30278b5003e96c3e4ad0dda17859581
abdurhmanmohamed <ysyb434@gmail.com>
2026-02-20 23:30:24 +0200

    Environments

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
