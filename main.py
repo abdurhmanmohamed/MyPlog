467c718bc1f9af8fd5d9e5114c1089e8738e1ff0
abdurhmanmohamed <ysyb434@gmail.com>
2026-02-21 03:48:50 +0200

    change the requirments file

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import os
 from flask import Flask, abort, render_template, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
-from flask_gravatar import Gravatar
 from flask_login import UserMixin, login_user, LoginManager, current_user, login_required,logout_user
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
