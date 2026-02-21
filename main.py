e60f5a5f2f52461d59cf030c565d668409953c24
abdurhmanmohamed <ysyb434@gmail.com>
2026-02-21 05:08:15 +0200

    d

--- main.py
+++ main.py
@@ -236,5 +236,4 @@ def contact():
 def show_work(id):
     writer = db.get_or_404(User, id)
     return render_template('writer_work.html' , posts = writer.posts)
-if __name__ == "__main__":
-    app.run(debug=False, port =5432 )
+
